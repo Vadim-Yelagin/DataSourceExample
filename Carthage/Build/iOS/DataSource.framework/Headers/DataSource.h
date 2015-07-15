36fb1a8ed55167351cda9a50de32c71bf882c89c
Vadim Yelagin <vadim@fueled.com>
2015-07-15 14:42:04 +0600

    Add DataSource

---
+++ Carthage/Build/iOS/DataSource.framework/Headers/DataSource.h
@@ -0,0 +1,9 @@
+//
+//  Copyright (c) 2015 Fueled. All rights reserved.
+//
+
+/// Project version number for DataSource.
+extern double DataSourceVersionNumber;
+
+/// Project version string for DataSource.
+extern const unsigned char DataSourceVersionString[];
