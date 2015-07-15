addae654bc0502c83e4911d4130be628e7dea99b
Vadim Yelagin <vadim@fueled.com>
2015-07-15 15:05:21 +0600

    Remove Carthage/Build

--- Carthage/Build/iOS/DataSource.framework/Headers/DataSource.h
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright (c) 2015 Fueled. All rights reserved.
-//
-
-/// Project version number for DataSource.
-extern double DataSourceVersionNumber;
-
-/// Project version string for DataSource.
-extern const unsigned char DataSourceVersionString[];
