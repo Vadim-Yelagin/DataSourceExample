128296f3509133da1edf98ac84224a7392fef0ef
Vadim Yelagin <vadim@fueled.com>
2015-07-17 01:03:19 +0600

    .preferredDisplayMode = .AllVisible

---
+++ DataSourceExample/Helpers/SplitHackViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplitHackViewController.swift
+//  DataSourceExample
+//
+//  Created by Vadim Yelagin on 17/07/15.
+//  Copyright (c) 2015 Fueled. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplitHackViewController: UISplitViewController {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.preferredDisplayMode = .AllVisible
+    }
+    
+}
