1d785cf243a8dbb8b2f99e36faef583d0a43ced7
Vadim Yelagin <vadim@fueled.com>
2015-07-17 00:07:11 +0600

    Minor changes

--- DataSourceExample/AppDelegate.swift
+++ DataSourceExample/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
     {
+        self.window?.tintColor = UIColor(red: 0.75, green: 0, blue: 0, alpha: 1)
         return true
     }
 
