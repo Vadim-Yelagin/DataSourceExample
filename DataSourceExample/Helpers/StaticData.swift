28d471e9c1a2728867d13c5483662a3fc2a37c07
Vadim Yelagin <vadim@fueled.com>
2015-07-17 01:13:31 +0600

    Tune StaticData

--- DataSourceExample/Helpers/StaticData.swift
+++ DataSourceExample/Helpers/StaticData.swift
@@ -10,14 +10,14 @@ import Foundation
 
 struct StaticData {
     
-    static let items = (0 ..< 5).map { ExampleItem(RandomData.spell($0)) }
+    static let items = (0 ..< 7).map { ExampleItem(RandomData.spell(126 + 113 * $0)) }
     
     static func randomItem() -> ExampleItem {
         return items[random(items.count)]
     }
     
     static func randomItems() -> [ExampleItem] {
-        let n = 1 + random(7)
+        let n = 3 + random(7)
         return (0 ..< n).map { _ in self.randomItem() }
     }
     
